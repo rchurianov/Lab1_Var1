e8fad390b05878411d69737519b1b35f8a226029
rchurianov <rchurianov@gmail.com>
2016-12-24 21:05:28 +0300

    Added test file for Program Class.

---
+++ Test_Lab1_Var1/ProgramTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Lab1_Var1
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void Test_Program()
+        {
+
+        }
+    }
+}
